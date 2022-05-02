f2b28e05058677cb40ff1b71bc3cdbeddd8720de
Mikael Andersson <mikael.andersson3@ingka.ikea.com>
2022-05-02 12:02:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -56,6 +56,7 @@ async function run() {
     var workspacePath = path.resolve(workspace);
     const projectFiles = recFindProjectJson(workspacePath);
     console.log(projectFiles);
+    
     projectFiles.forEach(scanForPrereleaseDependency);
     core.setOutput('time', new Date().toTimeString());
   } catch (error) {
