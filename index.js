4947b76e2eb37736aed93fff628e5ced203d7e10
ML916 <ML916@users.noreply.github.com>
2022-05-02 19:40:32 +0200

    feat: Created projectWithPreReleaseDependency array

--- index.js
+++ index.js
@@ -4,19 +4,12 @@ var path = require('path');
 const fs = require('fs');
 const wait = require('./wait');
 
-/*async function findProjectJsonFiles(workspace)
-{
-  var projectFiles = [];
-  console.log('Checking workspace at: ' + workspace);
-  var workspacePath = path.resolve(workspace);
-  console.log('Workspace path: ' + workspacePath);
-
-  var workspaceContents = fs.readdirSync(workspacePath, function(err,list));
-  console.log('Workspace contents: ' + workspaceContents);
-
-  return projectFiles;
-}*/
-
+/*
+* Recursively iterates through the base folder path to find any project.json files.
+* base = base directory to look in
+* files = list of files
+* result = list of project.json files
+*/
 function recFindProjectJson(base,files,result) 
 {
   files = files || fs.readdirSync(base) 
@@ -41,32 +34,53 @@ function recFindProjectJson(base,files,result)
   return result
 }
 
-async function scanForPrereleaseDependency(projectJsonFile)
+function hasPrereleaseDependency(projectJsonFile)
 {
   console.log('Scanning: ' + projectJsonFile);
-  var hasPrereleaseDependency = false;
   var projectJsonFilePath = path.resolve(projectJsonFile);
   var projectRawData = fs.readFileSync(projectJsonFilePath);
   var parsedProjectData = JSON.parse(projectRawData);
   
   var dependencies = parsedProjectData['dependencies'];
-  console.log("Project " + parsedProjectData["name"] + " has the following dependencies: " + dependencies);
+
   Object.entries(dependencies).map(item => {
-    console.log(item[0] + ": " + item[1])
-    core.setFailed(item[1])
+    console.log(item[0] + ": " + item[1]);
+    //core.setFailed(item[1]);
   });
+
+  const projectDependencyInfo = { name: parsedProjectData['name'], hasPrereleaseDependencies: true, dependencies:dependencies}
+  return projectDependencyInfo;
 }
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
     const workspace = core.getInput('workspace');
+    const errorLevel = core.getInput('errorLevel');
     //const projectFiles = await findProjectJsonFiles(workspace);
     var workspacePath = path.resolve(workspace);
     const projectFiles = recFindProjectJson(workspacePath);
     console.log(projectFiles);
+    
+    var projectsWithPrereleaseDependencies = [];
+    projectFiles.forEach(project => {
+      var projectDependencyInfo = hasPrereleaseDependency(project);
+      if(projectDependencyInfo['hasPrereleaseDependencies'] == true) {
+        projectsWithPrereleaseDependencies.push(projectDependencyInfo);
+      }
+    });
 
-    projectFiles.forEach(scanForPrereleaseDependency);
+    if(projectsWithPrereleaseDependencies.length > 0 && errorLevel == '#error') {
+      core.setFailed(projectsWithPrereleaseDependencies.toString());
+    } else if(projectsWithPrereleaseDependencies.length > 0 && errorLevel == '#warn'){
+      core.notice();
+    } else {
+      console.log('No prerelease dependencies were found.');
+    }
+
+    console.log(projectsWithPrereleaseDependencies);
+
+    //projectFiles.forEach(hasPrereleaseDependency);
     core.setOutput('time', new Date().toTimeString());
   } catch (error) {
     core.setFailed(error.message);
